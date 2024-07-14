fd3fd3ffe76951956fded8c2057a6c7ce16b5f74
刘敏皓 <lmh200019@gmail.com>
2024-07-14 14:43:00 +0800

    test1 使用TCP协议，多次发送消息，多次接收消息

---
+++ src/main/java/com/liuminhao/test1/Client.java
@@ -0,0 +1,45 @@
+package com.liuminhao.test1;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    /**
+     * 程序的入口点。
+     * 创建一个Socket客户端，连接到本地主机的指定端口，然后通过标准输入流接收用户输入，
+     * 并将输入内容发送到服务器，直到输入"886"为止，然后关闭Socket连接。
+     *
+     * @param args 命令行参数，本程序中未使用。
+     * @throws IOException 如果发生I/O错误。
+     */
+    public static void main(String[] args) throws IOException {
+        // 连接到本地主机的9999端口。
+        Socket socket = new Socket("127.0.0.1",9999);
+
+        // 通过标准输入流读取用户输入。
+        Scanner s = new Scanner(System.in);
+
+        // 获取Socket的输出流，用于向服务器发送数据。
+        OutputStream outputStream = socket.getOutputStream();
+
+        // 不断循环，等待用户输入。
+        while(true){
+            // 提示用户输入。
+            System.out.println("请输入：");
+            // 读取用户输入的一行。
+            String str = s.nextLine();
+            // 如果输入的是"886"，则退出循环。
+            if("886".equals(str)){
+                break;
+            }
+            // 将用户输入转换为字节数组并写入输出流，发送给服务器。
+            outputStream.write(str.getBytes());
+        }
+
+        // 关闭Socket连接。
+        socket.close();
+    }
+
+}
---
+++ src/main/java/com/liuminhao/test1/Server.java
@@ -0,0 +1,40 @@
+package com.liuminhao.test1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    /**
+     * 程序的入口点，用于启动一个简单的TCP服务器，监听指定端口并接收客户端发送的数据。
+     * @param args 命令行参数，本程序中未使用。
+     * @throws IOException 如果发生I/O错误。
+     */
+    public static void main(String[] args) throws IOException {
+        // 创建一个ServerSocket对象，监听9999端口。
+        ServerSocket serverSocket = new ServerSocket(9999);
+
+        // 等待客户端连接，接受连接并返回Socket对象。
+        Socket socket = serverSocket.accept();
+
+        // 获取客户端发送的数据流。
+        InputStream inputStream = socket.getInputStream();
+
+        // 将输入流包装为 InputStreamReader，指定字符集为默认字符集。
+        InputStreamReader reader = new InputStreamReader(inputStream);
+
+        // 逐行读取客户端发送的数据，并打印到控制台。
+        int line ;
+        while ((line = reader.read()) != -1) {
+            System.out.print((char) line);
+        }
+
+        // 关闭Socket和ServerSocket，释放资源。
+        socket.close();
+        serverSocket.close();
+    }
+
+}
